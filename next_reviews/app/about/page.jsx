e2d81f3b89497864884aa21e3a41c9277af3d547
NikhilReddyTondapu <nikhil8nr@gmail.com>
2024-09-28 00:38:58 +0530

    Part 12: Adding Review Links in Next.js: A Simple Exercise

--- next_reviews/app/about/page.jsx
+++ next_reviews/app/about/page.jsx
@@ -1,4 +1,4 @@
-export default function ReviewsPage() {
+export default function AboutPage() {
     return (
       <>
         <h1>About</h1>
---
+++ next_reviews/app/reviews/hollow-knight/page.jsx
@@ -0,0 +1,8 @@
+export default function HollowKnightPage() {
+    return (
+      <>
+        <h1>Hollow Knight</h1>
+        <p>[Placeholder for review content]</p>
+      </>
+    );
+  }
