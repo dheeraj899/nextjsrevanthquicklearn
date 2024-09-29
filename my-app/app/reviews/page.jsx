e62f4014bb3a1efaf2fbca51beaa98c90463aa5b
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-09-29 16:00:51 +0530

    Part 13: Exploring Styling Solutions in Next.js

--- my-app/app/reviews/page.jsx
+++ my-app/app/reviews/page.jsx
@@ -1,9 +1,11 @@
 import Link from 'next/link';
+import styles from './Reviews.module.css';
 export default function ReviewsPage() {
   return (
     <>
       <h1>Reviews</h1>
       <p>Here we'll list all the reviews.</p>
+      <ul className={styles.list}></ul>
       <ul>
         <li>
           <Link href="/reviews/hollow-knight">Hollow Knight</Link>
