a775e9b0102294611d5a71341e81731de22aaeda
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-09-29 16:21:41 +0530

    Part 14: How to Install and Configure Tailwind CSS in Your Next.js Project

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -1,9 +1,13 @@
+import ExampleComponent from './components/ExampleComponent';
+
 export default function HomePage() {
   console.log('[HomePage] rendering');
   return (
     <>
       <h1>Indie Gamer</h1>
       <p>Only the best indie games, reviewed for you.</p>
+      {/* Adding ExampleComponent here */}
+      <ExampleComponent />
     </>
   );
 }
