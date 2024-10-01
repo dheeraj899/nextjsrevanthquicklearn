fa2da4402583bfd04058a29c4f377b24b1c0e724
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-10-01 15:41:33 +0530

    Part 23: Customizing Default Fonts in Tailwind CSS with Next.js

--- my-app/app/reviews/page.jsx
+++ my-app/app/reviews/page.jsx
@@ -1,32 +1,24 @@
-import Link from 'next/link';
-import Heading from '@/components/Heading';
-
 export default function ReviewsPage() {
   return (
-    <>
-      <Heading>Reviews</Heading>
-      <ul className="flex flex-col gap-3">
-        <li className="bg-white border rounded shadow w-80 hover:shadow-xl">
-          <Link href="/reviews/hollow-knight">
-            <img src="/images/hollow-knight.jpg" alt="Hollow Knight"
-              width="320" height="180" className="rounded-t"
-            />
-            <h2 className="font-orbitron font-semibold py-1 text-center">
-              Hollow Knight
-            </h2>
-          </Link>
-        </li>
-        <li className="bg-white border rounded shadow w-80 hover:shadow-xl">
-          <Link href="/reviews/stardew-valley">
-            <img src="/images/stardew-valley.jpg" alt="Stardew Valley"
-              width="320" height="180" className="rounded-t"
-            />
-            <h2 className="font-orbitron font-semibold py-1 text-center">
-              Stardew Valley
-            </h2>
-          </Link>
-        </li>
-      </ul>
-    </>
+    <div className="flex flex-col gap-4">
+      <Link href="/review/hollow-knight" className="block">
+        <img src="/images/hollow-knight.jpg" alt="" width="320" height="180" className="rounded-t" />
+        <h2 className="font-orbitron font-semibold py-1 text-center">
+          Hollow Knight
+        </h2>
+        <p className="text-justify">
+          The Hollow Knight is an incredible game that takes you on a journey through a beautifully crafted world...
+        </p>
+      </Link>
+      <Link href="/review/stardew-valley" className="block">
+        <img src="/images/stardew-valley.jpg" alt="" width="320" height="180" className="rounded-t" />
+        <h2 className="font-orbitron font-semibold py-1 text-center">
+          Stardew Valley
+        </h2>
+        <p className="text-justify">
+          Stardew Valley is a charming farming simulation game that allows you to build and manage your own farm...
+        </p>
+      </Link>
+    </div>
   );
 }
