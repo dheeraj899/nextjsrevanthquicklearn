7106a616c11a0d633b094dcdf3a6ee259dd670f7
NikhilReddyTondapu <nikhil8nr@gmail.com>
2024-10-01 12:20:19 +0530

    Part 19: Enhancing Your Next.js Project with Static Assets

--- next_reviews/app/reviews/hollow-knight/page.jsx
+++ next_reviews/app/reviews/hollow-knight/page.jsx
@@ -1,9 +1,17 @@
-//import Heading from '@/components/Heading';
-import Heading from "@/app/components/Heading";
+// app/reviews/hollow-knight/page.jsx
+import Heading from '@/components/Heading';
+
 export default function HollowKnightPage() {
   return (
     <>
       <Heading>Hollow Knight</Heading>
+      <img 
+        src="/images/hollow-knight.jpg" 
+        alt="Hollow Knight" 
+        width="640" 
+        height="360" 
+        className="mb-2 rounded" 
+      />
       <p>
         This will be the review for Hollow Knight.
       </p>
