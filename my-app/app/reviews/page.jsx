97c1258c3f28f5c1ca8ee8cc64835ff96f977030
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-10-01 12:36:17 +0530

    Part 20: Enhancing Your Reviews Page with Image Cards in Next.js

--- my-app/app/reviews/page.jsx
+++ my-app/app/reviews/page.jsx
@@ -1,20 +1,30 @@
 import Link from 'next/link';
 import Heading from '@/components/Heading';
 
-import styles from './Reviews.module.css';
 export default function ReviewsPage() {
   return (
     <>
-    <Heading /> {/* Render the Heading component here */}
       <Heading>Reviews</Heading>
-      <p>Here we'll list all the reviews.</p>
-      <ul className={styles.list}></ul>
-      <ul>
-        <li>
-          <Link href="/reviews/hollow-knight">Hollow Knight</Link>
+      <ul className="flex flex-col gap-3">
+        <li className="bg-white border rounded shadow w-80 hover:shadow-xl">
+          <Link href="/reviews/hollow-knight">
+            <img src="/images/hollow-knight.jpg" alt="Hollow Knight"
+              width="320" height="180" className="rounded-t"
+            />
+            <h2 className="py-1 text-center">
+              Hollow Knight
+            </h2>
+          </Link>
         </li>
-        <li>
-          <Link href="/reviews/stardew-valley">Stardew Valley</Link>
+        <li className="bg-white border rounded shadow w-80 hover:shadow-xl">
+          <Link href="/reviews/stardew-valley">
+            <img src="/images/stardew-valley.jpg" alt="Stardew Valley"
+              width="320" height="180" className="rounded-t"
+            />
+            <h2 className="py-1 text-center">
+              Stardew Valley
+            </h2>
+          </Link>
         </li>
       </ul>
     </>
