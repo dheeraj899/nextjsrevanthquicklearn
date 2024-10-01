9a50a44a573addee293295c5de1811308c86fda6
NikhilReddyTondapu <nikhil8nr@gmail.com>
2024-10-01 16:04:32 +0530

    Part 22: Integrating Custom Fonts with Tailwind CSS in Next.js

--- next_reviews/app/components/Heading.jsx
+++ next_reviews/app/components/Heading.jsx
@@ -1,8 +1,8 @@
 // components/Heading.jsx
-import { orbitron } from '../fonts';
+//import { orbitron } from '../fonts';
 export default function Heading({ children }) {
   return (
-    <h1 className={`font-bold text-2xl pb-3 ${orbitron.className}`}>
+    <h1 className="font-bold font-orbitron pb-3 text-2xl">
       {children}
     </h1>
   );
--- next_reviews/app/layout.jsx
+++ next_reviews/app/layout.jsx
@@ -1,8 +1,9 @@
 import NavBar from './components/NavBar';
 import './globals.css';
+import { orbitron } from './fonts';
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en"  className={orbitron.variable}>
       <body className="bg-orange-50 flex flex-col px-4 py-2 min-h-screen">
         <header>
           <NavBar />
