05fd965de738edd95b85e9f9d35df13c5c370211
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-10-01 13:04:18 +0530

    Part 21: Font Optimization in Next.js: A Step-by-Step Guide

--- my-app/app/components/Heading.jsx
+++ my-app/app/components/Heading.jsx
@@ -1,6 +1,8 @@
+import { orbitron } from '@/fonts'; // Updated import path
+
 export default function Heading({ children }) {
   return (
-    <h1 className="font-bold text-2xl pb-3">
+    <h1 className={`font-bold text-2xl pb-3 ${orbitron.className}`}>
       {children}
     </h1>
   );
