a9df5fb065fcfc39cf4fd89d40c98c16ca4c5c0f
LAPTOP-2HH5FS0T\siddh <sankaramgariabbayyi2002@gmail.com>
2024-10-05 11:39:50 +0530

    Part 5: Understanding Nested Layouts in Next.js

--- my-app/app/layout.jsx
+++ my-app/app/layout.jsx
@@ -2,7 +2,9 @@ export default function RootLayout({ children }) {
     return (
       <html lang="en">
         <body>
-          {children}
+          <header>[Header]</header>
+          <main>{children}</main>
+          <footer>[Footer]</footer>
         </body>
       </html>
     );
---
+++ my-app/app/reviews/layout.jsx
@@ -0,0 +1,8 @@
+export default function ReviewsLayout({ children }) {
+    return (
+      <div>
+        <div style={{ border: '2px solid red' }}>Reviews MenuBar</div>
+        {children}
+      </div>
+    );
+  }
