081b1b7dc42632fdb62d594605e66573b1ee95bd
LAPTOP-2HH5FS0T\siddh <sankaramgariabbayyi2002@gmail.com>
2024-10-07 11:20:29 +0530

    Part 7: Exploring React Server Components in Next.js

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -1,9 +1,17 @@
+// HomePage.jsx
+'use client';
+
+import { useEffect } from 'react';
+
 export default function HomePage() {
-    console.log('[HomePage] rendering');
-    return (
-      <>
-        <h1>Indie Gamer</h1>
-        <p>Welcome to the Indie Gamer website!</p>
-      </>
-    );
-  }
+  useEffect(() => {
+    window.alert('Welcome to my site!!!');
+  }, []);
+
+  return (
+    <>
+      <h1>Indie Gamer</h1>
+      <p>Welcome to the Indie Gamer website!</p>
+    </>
+  );
+}
