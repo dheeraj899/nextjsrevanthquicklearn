dd7ecb403d8dd56d886737f6885ef3dca16df28f
LAPTOP-2HH5FS0T\siddh <sankaramgariabbayyi2002@gmail.com>
2024-10-07 11:39:29 +0530

    Part 9: Enhancing Navigation with the Link Component in Next.js

--- my-app/app/layout.jsx
+++ my-app/app/layout.jsx
@@ -1,11 +1,20 @@
+import Link from 'next/link';
+
 export default function RootLayout({ children }) {
-    return (
-      <html lang="en">
-        <body>
-          <header>[Header]</header>
-          <main>{children}</main>
-          <footer>[Footer]</footer>
-        </body>
-      </html>
-    );
-  }
+  return (
+    <html lang="en">
+      <body>
+        <header>
+          <nav>
+            <ul>
+              <li><Link href="/">Home</Link></li>
+              <li><Link href="/reviews">Reviews</Link></li>
+              <li><Link href="/about">About</Link></li>
+            </ul>
+          </nav>
+        </header>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
