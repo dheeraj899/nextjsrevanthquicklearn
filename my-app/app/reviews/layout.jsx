cc6631bc59eafd583e34e2d6f4b88ea72cfc15b8
LAPTOP-2HH5FS0T\siddh <sankaramgariabbayyi2002@gmail.com>
2024-10-08 09:58:03 +0530

    Part 12: Adding Review Links in Next.js: A Simple Exercise

--- my-app/app/reviews/layout.jsx
+++ my-app/app/reviews/layout.jsx
@@ -1,8 +1,17 @@
-export default function ReviewsLayout({ children }) {
-    return (
-      <div>
-        <div style={{ border: '2px solid red' }}>Reviews MenuBar</div>
-        {children}
-      </div>
-    );
-  }
+import Link from 'next/link';
+export default function ReviewsPage() {
+  return (
+    <>
+      <h1>Reviews</h1>
+      <p>Here we will list all the reviews.</p>
+      <ul>
+        <li>
+          <Link href="/reviews/hollow-knight">Hollow Knight</Link>
+        </li>
+        <li>
+          <Link href="/reviews/stardew-valley">Stardew Valley</Link>
+        </li>
+      </ul>
+    </>
+  );
+}
