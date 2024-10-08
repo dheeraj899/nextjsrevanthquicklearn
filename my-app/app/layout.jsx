00f1e5c57706f5add38839222ea98bcf11784bf9
LAPTOP-2HH5FS0T\siddh <sankaramgariabbayyi2002@gmail.com>
2024-10-08 09:48:13 +0530

    Part 11: Enhancing Your Next.js Application with Layout and Navigation

--- my-app/app/layout.jsx
+++ my-app/app/layout.jsx
@@ -9,11 +9,14 @@ export default function RootLayout({ children }) {
             <ul>
               <li><Link href="/">Home</Link></li>
               <li><Link href="/reviews">Reviews</Link></li>
-              <li><Link href="/about" prefetch={false}>About</Link></li>
+              <li><Link href="/about">About</Link></li>
             </ul>
           </nav>
         </header>
         <main>{children}</main>
+        <footer>
+          Quick Learn Blogs by <a href="https://www.revanthquicklearn.com/" target="_blank" rel="noopener noreferrer">Revanth</a>
+        </footer>
       </body>
     </html>
   );
--- my-app/app/reviews/page.jsx
+++ my-app/app/reviews/page.jsx
@@ -2,7 +2,7 @@ export default function ReviewsPage() {
     return (
       <>
         <h1>Reviews</h1>
-        <p>Here we'll list all the reviews.</p>
+        <p>Here we will list all the reviews.</p>
       </>
     );
   }
