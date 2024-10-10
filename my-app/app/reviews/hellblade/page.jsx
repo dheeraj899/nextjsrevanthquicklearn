977469639c64f2a7a27ab37971ca4fb317c0e616
LAPTOP-2HH5FS0T\siddh <sankaramgariabbayyi2002@gmail.com>
2024-10-10 12:20:38 +0530

    Part 19: Enhancing Your Next.js Project with Static Assets

---
+++ my-app/app/reviews/hellblade/page.jsx
@@ -0,0 +1,20 @@
+// app/reviews/hellblade/page.jsx (Placeholder)
+import Heading from '@/components/Heading';
+
+export default function HellbladePage() {
+  return (
+    <>
+      <Heading>Hellblade</Heading>
+      <img 
+        src="/images/hellblade.jpg" 
+        alt="Hellblade" 
+        width="640" 
+        height="360" 
+        className="mb-2 rounded" 
+      />
+      <p>
+        This will be the review for Hellblade.
+      </p>
+    </>
+  );
+}
