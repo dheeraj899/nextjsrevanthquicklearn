cb7c14558991d810dcc9108c7510da2f8ac8d764
LAPTOP-2HH5FS0T\siddh <sankaramgariabbayyi2002@gmail.com>
2024-10-10 11:52:14 +0530

    Part 16: Adding a Splash of Color with Tailwind CSS

--- my-app/app/layout.jsx
+++ my-app/app/layout.jsx
@@ -1,39 +1,24 @@
-import Link from 'next/link';
+// RootLayout.jsx
+import NavBar from '../components/NavBar';
 import './globals.css';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <title>Your App Title</title>
-        {/* Add any meta tags or links here */}
-      </head>
-      <body className="flex flex-col px-4 py-2 min-h-screen">
+      <body className="bg-orange-50 flex flex-col px-4 py-2 min-h-screen">
         <header>
-          <nav>
-            <ul className="flex gap-2">
-              <li><Link href="/">Home</Link></li>
-              <li><Link href="/reviews">Reviews</Link></li>
-              <li><Link href="/about">About</Link></li>
-              <li><Link href="/contact">Contact</Link></li>
-              {/* Add more links as needed */}
-            </ul>
-          </nav>
+          <NavBar />
         </header>
         <main className="grow py-3">
           {children}
         </main>
         <footer className="border-t py-3 text-center text-xs">
-          Game data and images courtesy of ...
-        </footer>
+  Quick Learn Blogs by <a href="https://www.revanthquicklearn.com/" target="_blank" className="text-blue-800 hover:underline">
+    Revanth
+  </a>
+</footer>
       </body>
     </html>
   );
 }
-
-
-
-
-
-
 
---
+++ my-app/components/NavBar.jsx
@@ -0,0 +1,20 @@
+// components/NavBar.jsx
+import Link from 'next/link';
+
+export default function NavBar() {
+  return (
+    <nav>
+      <ul className="flex gap-2">
+        <li>
+          <Link href="/" className="text-orange-800 hover:underline">Home</Link>
+        </li>
+        <li>
+          <Link href="/reviews" className="text-red-800 hover:underline">Reviews</Link>
+        </li>
+        <li>
+          <Link href="/about" prefetch={false} className="text-orange-800 hover:underline">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
