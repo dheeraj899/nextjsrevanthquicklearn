644a6aa5965bb28c7b35b7cd5bb78c8f98eee069
NikhilReddyTondapu <nikhil8nr@gmail.com>
2024-10-21 15:56:22 +0530

    Part 57: Refactoring API Calls in Next.js for Enhanced Code Reusability

--- next_reviews/app/lib/reviews.js
+++ next_reviews/app/lib/reviews.js
@@ -3,46 +3,51 @@ import matter from 'gray-matter';
 import { marked } from 'marked';
 import qs from 'qs';
 const CMS_URL = 'http://localhost:1337';
-
-export async function getReview(slug) {
+//lib/reviewes.js
+async function fetchReviews(parameters) {
   const url = `${CMS_URL}/api/reviews?`
-    + qs.stringify({
-      filters: { slug: { $eq: slug } },
-      fields: ['slug', 'title', 'subtitle', 'publishedAt', 'body'],
-      populate: { image: { fields: ['url'] } },
-      pagination: { pageSize: 1, withCount: false },
-    }, { encodeValuesOnly: true });
-
+    + qs.stringify(parameters, { encodeValuesOnly: true });
+  console.log('[fetchReviews]:', url);
   const response = await fetch(url);
-  const { data } = await response.json();
-
-  const { attributes } = data[0];
+  if (!response.ok) {
+    throw new Error(`CMS returned ${response.status} for ${url}`);
+  }
+  return await response.json();
+}
+function toReview(item) {
+  const { attributes } = item;
   return {
     slug: attributes.slug,
     title: attributes.title,
     date: attributes.publishedAt.slice(0, 'yyyy-mm-dd'.length),
     image: CMS_URL + attributes.image.data.attributes.url,
-    body: marked(attributes.body),
   };
 }
 export async function getReviews() {
-  const url = `${CMS_URL}/api/reviews?` + qs.stringify({
+  const { data } = await fetchReviews({
     fields: ['slug', 'title', 'subtitle', 'publishedAt'],
     populate: { image: { fields: ['url'] } },
     sort: ['publishedAt:desc'],
     pagination: { pageSize: 6 },
-  }, { encodeValuesOnly: true });
-
-  const response = await fetch(url);
-  const { data } = await response.json();
-
-  return data.map(({ attributes }) => ({
-    slug: attributes.slug,
-    title: attributes.title,
-    date: attributes.publishedAt.slice(0, 'yyyy-mm-dd'.length),
-    image: CMS_URL + attributes.image.data.attributes.url,
-  }));
+  });
+  return data.map(toReview);
 }
+
+
+export async function getReview(slug) {
+  const { data } = await fetchReviews({
+    filters: { slug: { $eq: slug } },
+    fields: ['slug', 'title', 'subtitle', 'publishedAt', 'body'],
+    populate: { image: { fields: ['url'] } },
+    pagination: { pageSize: 1, withCount: false },
+  });
+  const item = data[0];
+  return {
+    ...toReview(item),
+   body: marked(item.attributes.body,{headerIds: false, mangle: false}),
+  };
+}
+
   export async function getSlugs() {
   const files = await readdir('./app/content/reviews');
   return files.filter((file) => file.endsWith('.md'))
