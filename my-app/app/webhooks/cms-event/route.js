295807e487402b1103e718297a27113b6f6e0925
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-10-22 14:20:38 +0530

    Part 72: Enhancing Data Freshness with On-Demand Revalidation in Next.js

---
+++ my-app/app/webhooks/cms-event/route.js
@@ -0,0 +1,6 @@
+// app/webhooks/cms-event/route.js
+export async function POST(request) {
+  const payload = await request.json();
+  console.log('payload:', payload);
+  return new Response(null, { status: 204 });
+}
