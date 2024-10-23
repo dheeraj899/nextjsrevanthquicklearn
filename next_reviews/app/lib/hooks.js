b39dd33c89ccb4135c54d2de783ac335fc7bc323
NikhilReddyTondapu <nikhil8nr@gmail.com>
2024-10-23 16:44:18 +0530

    Part 80: Resolving Hydration Issues in Next.js with Custom Hooks

---
+++ next_reviews/app/lib/hooks.js
@@ -0,0 +1,10 @@
+// lib/hooks.js
+import { useEffect, useState } from 'react';
+
+export function useIsClient() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => setIsClient(true), []);
+
+  return isClient;
+}
