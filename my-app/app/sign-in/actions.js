d44f7e378792f457f0d14d9be96b62fa822d8b46
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-10-28 13:00:37 +0530

    Part 120: Enhancing Authentication with Cookies in Next.js

--- my-app/app/sign-in/actions.js
+++ my-app/app/sign-in/actions.js
@@ -2,16 +2,19 @@
 
 'use server';
 
+import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 export async function signInAction(formData) {
-  console.log('[signInAction]', formData);
   const email = formData.get('email');
   const password = formData.get('password');
   const user = authenticate(email, password);
+  
   if (!user) {
     return { isError: true, message: 'Invalid credentials' };
   }
+
+  cookies().set('user', JSON.stringify(user)); // Set the user cookie
   redirect('/');
 }
 
