61fe78bfc26b9c7c9e52bb382055a9ee8a5a4830
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-10-28 12:11:35 +0530

    Part 119: Building a Simple Authentication Flow with Next.js

--- my-app/app/sign-in/actions.js
+++ my-app/app/sign-in/actions.js
@@ -2,6 +2,21 @@
 
 'use server';
 
+import { redirect } from 'next/navigation';
+
 export async function signInAction(formData) {
   console.log('[signInAction]', formData);
+  const email = formData.get('email');
+  const password = formData.get('password');
+  const user = authenticate(email, password);
+  if (!user) {
+    return { isError: true, message: 'Invalid credentials' };
+  }
+  redirect('/');
+}
+
+function authenticate(email, password) {
+  if (email.endsWith('@example.com') && password === 'test') {
+    return { email };
+  }
 }
