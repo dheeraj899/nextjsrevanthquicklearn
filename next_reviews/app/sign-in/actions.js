8c7ee529f98de986507e51f91b5e973500a6acf2
NikhilReddyTondapu <nikhil8nr@gmail.com>
2024-10-28 15:37:57 +0530

    Part 121: Enhancing Security with JSON Web Tokens in Next.js

--- next_reviews/app/sign-in/actions.js
+++ next_reviews/app/sign-in/actions.js
@@ -2,19 +2,24 @@
 
 'use server';
 
+import { SignJWT } from 'jose';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
+const JWT_SECRET = new TextEncoder().encode('some-random-string');
+
 export async function signInAction(formData) {
+  console.log('[signInAction]', formData);
   const email = formData.get('email');
   const password = formData.get('password');
   const user = authenticate(email, password);
-  
   if (!user) {
     return { isError: true, message: 'Invalid credentials' };
   }
-
-  cookies().set('user', JSON.stringify(user)); // Set the user cookie
+  const sessionToken = await new SignJWT(user)
+    .setProtectedHeader({ alg: 'HS256' })
+    .sign(JWT_SECRET);
+  cookies().set('sessionToken', sessionToken);
   redirect('/');
 }
 
