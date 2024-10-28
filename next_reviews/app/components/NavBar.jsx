151b0d93ebd3b8ce7cfad1baa15fb44f44b804a3
NikhilReddyTondapu <nikhil8nr@gmail.com>
2024-10-28 15:33:53 +0530

    Part 120: Enhancing Authentication with Cookies in Next.js

--- next_reviews/app/components/NavBar.jsx
+++ next_reviews/app/components/NavBar.jsx
@@ -1,24 +1,40 @@
-// components/NavBar.jsx
+// File path: components/NavBar.jsx
+
+import { cookies } from 'next/headers';
 import NavLink from './NavLink';
 
 export default function NavBar() {
+  const userCookie = cookies().get('user');
+  const user = userCookie ? JSON.parse(userCookie.value) : null;
   return (
     <nav>
       <ul className="flex gap-2">
-      <li>
+        <li className="font-bold font-orbitron">
+          <NavLink href="/">
+            Indie Gamer
+          </NavLink>
+        </li>
+        <li className="ml-auto">
+          <NavLink href="/reviews">
+            Reviews
+          </NavLink>
+        </li>
+        <li>
+          <NavLink href="/about" prefetch={false}>
+            About
+          </NavLink>
+        </li>
+        {user ? (
+          <li>
+            {user.email}
+          </li>
+        ) : (
+        <li>
           <NavLink href="/sign-in">
             Sign in
           </NavLink>
         </li>
-        <li className="font-bold font-orbitron">
-          <NavLink href="/">Indie Gamer</NavLink>
-        </li>
-        <li className="ml-auto">
-          <NavLink href="/reviews">Reviews</NavLink>
-        </li>
-        <li>
-          <NavLink href="/about" prefetch={false}>About</NavLink>
-        </li>
+        )}
       </ul>
     </nav>
   );
