3dd07729f5c77c93296a2e48ececcd29c5b40c22
NikhilReddyTondapu <nikhil8nr@gmail.com>
2024-10-29 16:37:40 +0530

    Part 130: Securing User Passwords with Bcrypt: A Step-by-Step Guide

--- next_reviews/app/lib/users.js
+++ next_reviews/app/lib/users.js
@@ -1,17 +1,20 @@
 // File path: lib/users.js
 
-// File path: lib/users.js
-
+import { hash } from 'bcrypt';
 import { db } from './db';
-
-export async function authenticateUser(email, password) {
-  return await db.user.findUnique({
-    where: { email, password },
-  });
-}
+import { compare } from 'bcrypt';
 
 export async function createUser({ email, name, password }) {
+  const passwordHash = await hash(password, 10); // 10 rounds for salt generation
   return await db.user.create({
-    data: { email, name, password },
+    data: { email, name, passwordHash },
   });
 }
+export async function authenticateUser(email, password) {
+    const user = await db.user.findUnique({
+      where: { email },
+    });
+    if (user && await compare(password, user.passwordHash)) {
+      return user;
+    }
+  }
