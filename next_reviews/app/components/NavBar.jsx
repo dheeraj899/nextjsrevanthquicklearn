8c3ad4c3910eac49bd84486eed08da5a1c73747c
NikhilReddyTondapu <nikhil8nr@gmail.com>
2024-10-29 15:20:30 +0530

    Part 122: Securing User Authentication with JSON Web Tokens

--- next_reviews/app/components/NavBar.jsx
+++ next_reviews/app/components/NavBar.jsx
@@ -1,11 +1,28 @@
 // File path: components/NavBar.jsx
 
+import { jwtVerify } from 'jose';
 import { cookies } from 'next/headers';
 import NavLink from './NavLink';
 
-export default function NavBar() {
-  const userCookie = cookies().get('user');
-  const user = userCookie ? JSON.parse(userCookie.value) : null;
+const JWT_SECRET = new TextEncoder().encode('some-random-string');
+
+async function getUserFromSession() {
+  const sessionTokenCookie = cookies().get('sessionToken');
+  if (sessionTokenCookie) {
+    try {
+      const { payload } = await jwtVerify(sessionTokenCookie.value, JWT_SECRET);
+      return payload;
+    } catch (error) {
+      console.warn('Invalid JWT', error);
+    }
+  }
+}
+
+
+// File path: components/NavBar.jsx
+
+export default async function NavBar() {
+  const user = await getUserFromSession();
   return (
     <nav>
       <ul className="flex gap-2">
