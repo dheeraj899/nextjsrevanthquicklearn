7b7bb4e1da47bbe493af574b11003a38e55c309d
NikhilReddyTondapu <nikhil8nr@gmail.com>
2024-10-29 15:36:30 +0530

    Part 124: Enhancing Security in Authentication: Setting Expiry and Attributes for JWTs and Cookies

--- next_reviews/app/lib/auth.js
+++ next_reviews/app/lib/auth.js
@@ -5,6 +5,10 @@ import { cookies } from 'next/headers';
 
 const JWT_COOKIE = 'sessionToken';
 const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET);
+// File path: lib/auth.js
+
+const JWT_DURATION = 14 * 24 * 60 * 60 * 1000; // 2 weeks
+
 
 export async function getUserFromSession() {
   const sessionTokenCookie = cookies().get(JWT_COOKIE);
@@ -20,8 +24,16 @@ export async function getUserFromSession() {
 // File path: lib/auth.js
 
 export async function setSessionCookie(user) {
+    const expirationTime = new Date(Date.now() + JWT_DURATION);
     const sessionToken = await new SignJWT(user)
-      .setProtectedHeader({ alg: 'HS256' })
-      .sign(JWT_SECRET);
-    cookies().set(JWT_COOKIE, sessionToken);
+    //const expirationTime = new Date(Date.now() + JWT_DURATION);
+    .setProtectedHeader({ alg: 'HS256' })
+    .setExpirationTime(expirationTime)
+    .sign(JWT_SECRET);
+    cookies().set(JWT_COOKIE, sessionToken, {
+        expires: expirationTime,
+        httpOnly: true,
+        sameSite: 'lax',
+      });
+    
   }
